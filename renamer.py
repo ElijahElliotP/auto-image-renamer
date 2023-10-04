9af5a420cca92f4e1cdc29eb0135c422fe8a52ee
Elijah <elijah.perez@gmail.com>
2023-10-04 09:51:43 -0400

    an ignore to pull the keys out of the script

--- renamer.py
+++ renamer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import skeys
 import http.client
 import json
 import os
@@ -9,8 +10,8 @@ from os import listdir
 from os.path import isfile, join
 
 # Change here
-MICROSOFT_VISION_API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-MICROSOFT_VISION_API_ENDPOINT = 'eastus.api.cognitive.microsoft.com'
+MICROSOFT_VISION_API_KEY = skeys.key1
+MICROSOFT_VISION_API_ENDPOINT = skeys.endpoint1
 
 ALLOWED_IMAGE_EXTENSIONS = ['.jpeg', '.jpg', '.png']
 
@@ -35,12 +36,33 @@ def get_extension(file):
     return ext
 
 
-def rename_img(old, new, base_dir):
+def find_available_filename(base_dir, new, ext):
+    # Define the initial filename
+    filename = os.path.join(base_dir, f"{new}{ext}")
+    
+    # Check if the file already exists
+    if os.path.isfile(filename):
+        # If it exists, increment the filename until an available one is found
+        i = 1
+        while True:
+            new_filename = os.path.join(base_dir, f"{new}_{i}{ext}")
+            if not os.path.isfile(new_filename):
+                return new_filename
+            i += 1
+    else:
+        # If the file doesn't exist, return the original filename
+        return filename
+
+
+def rename_img(old, new, base_dir): #old, join(base_dir, new + ext)
     if is_exists(old):
         ext = get_extension(old).lower()
-        os.rename(old, join(base_dir, new + ext))
-        print("Renaming ", old, "to ", new + ext)
-
+        if ext is not None:
+            # os.rename(old, join(base_dir, new + ext))
+            os.rename(old, find_available_filename(base_dir, new, ext))
+            print("Renaming ", old, "to ", new + ext)
+        else: 
+            print("File extension is type None. Skipping.")
 
 def get_caption(image_file):
     headers = {
@@ -58,9 +80,14 @@ def get_caption(image_file):
         conn = http.client.HTTPSConnection(MICROSOFT_VISION_API_ENDPOINT)
         conn.request("POST", "/vision/v3.2/describe?%s" % params, data, headers)
         response = conn.getresponse()
+        # print(response)
         data = response.read()
+        # print(data)
         json_data = json.loads(data)
         caption_text = json_data['description']['captions'][0]['text']
+        print(caption_text)
+        caption_text = caption_text.replace(",","").replace(" ","_")
+        print(caption_text)
         conn.close()
         return caption_text
     except Exception as e:
